7fd5923dedff798d12c31c5dc7469aa96c608b0d
roshannp <43735158+roshannp@users.noreply.github.com>
2025-02-15 10:04:51 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.testserver.id
+  value       = aws_instance.test_server.id
 }
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.testserver.public_ip
+  value       = aws_instance.test_server.public_ip
 }
